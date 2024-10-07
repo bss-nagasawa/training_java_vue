65e1ff6b9a5a10fd1d2a985ca057b7a8a299bc07
swr0408 <swr0408@yahoo.co.jp>
2024-10-07 15:00:38 +0900

    エンコード処理追加

--- sql/admin_login_info.sql
+++ sql/admin_login_info.sql
@@ -5,4 +5,5 @@ CREATE TABLE admin_login_info (
     auth INT(1) NOT NULL COMMENT '権限',
     regist_date TIMESTAMP NOT NULL COMMENT '登録日',
     register VARCHAR(20) NOT NULL COMMENT '管理者名'
-) COMMENT = '管理機能ログイン情報';
+) COMMENT = '管理機能ログイン情報'
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- sql/user_login_info.sql
+++ sql/user_login_info.sql
@@ -5,4 +5,5 @@ CREATE TABLE user_login_info (
     category INT(1) NULL COMMENT '権限',
     regist_date TIMESTAMP NOT NULL COMMENT '登録日',
     update_date TIMESTAMP NULL COMMENT '更新日'
-)COMMENT = '会員機能ログイン情報';
+)COMMENT = '会員機能ログイン情報'
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
