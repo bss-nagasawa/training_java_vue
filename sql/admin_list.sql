35f5c04ef72d81b29edea1eb85e312e024c633f1
swr0408 <swr0408@yahoo.co.jp>
2024-10-07 18:26:48 +0900

    各レコードのregist_dateの入力を平文に修正。

--- sql/admin_list.sql
+++ sql/admin_list.sql
@@ -1,6 +1,6 @@
 INSERT INTO admin_login_info (login_id, password, auth, regist_date, register) 
 VALUES 
-('f2dvd7ft46', 's4xwH6PNreTHiXaupJc6', 1, NOW(), '佐藤 一郎'),
-('6g5wqu5jpk', 'B3dB3JKxLz2sKhVFtGhn', 1, NOW(), '田中 太郎'),
-('qpkjj528rc', 'ah95asLiDYsLue4PxRbq', 2, NOW(), '田村 正二'),
-('bdg2k7dvd7', 'HWDag7L3JRUyJQTW6cpU', 2, NOW(), '清水 次郎');
+('f2dvd7ft46', 's4xwH6PNreTHiXaupJc6', 1, '2024-10-07 12:30:00', '佐藤 一郎'),
+('6g5wqu5jpk', 'B3dB3JKxLz2sKhVFtGhn', 1, '2024-10-07 12:30:00', '田中 太郎'),
+('qpkjj528rc', 'ah95asLiDYsLue4PxRbq', 2, '2024-10-07 12:30:00', '田村 正二'),
+('bdg2k7dvd7', 'HWDag7L3JRUyJQTW6cpU', 2, '2024-10-07 12:30:00', '清水 次郎');
